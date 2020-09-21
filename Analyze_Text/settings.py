a24b7f5026165b8b9a3daac8b9baa5f587b93fcd
Brajeswar Lenka <brajeswar.lenka@gmail.com>
2020-09-21 20:59:26 +0530

    Update settings.py

--- Analyze_Text/settings.py
+++ Analyze_Text/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'g)s^7_f%ee+#(rb^@xow@%#j+*av495(*tf1ls&73zgad+_g(%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
